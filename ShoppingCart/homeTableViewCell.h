24e8641baa19496223c837562f1edd22073b5003
maki toda <earth.in.us@gmail.com>
2017-03-11 10:17:21 -0800

    Created homeTableViewCell files.

---
+++ ShoppingCart/homeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  homeTableViewCell.h
+//  ShoppingCart
+//
+//  Created by 戸田 真紀 on 2017-03-11.
+//  Copyright © 2017 CICCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface homeTableViewCell : UITableViewCell
+
+@end
---
+++ ShoppingCart/homeTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  homeTableViewCell.m
+//  ShoppingCart
+//
+//  Created by 戸田 真紀 on 2017-03-11.
+//  Copyright © 2017 CICCC. All rights reserved.
+//
+
+#import "homeTableViewCell.h"
+
+@implementation homeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
