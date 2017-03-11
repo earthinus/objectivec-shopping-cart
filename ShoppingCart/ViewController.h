b4e222a7e9f4865d6f571ee8a3dd373a9c44b171
maki toda <earth.in.us@gmail.com>
2017-03-11 10:17:02 -0800

    Added homeTableView.

--- ShoppingCart/ViewController.h
+++ ShoppingCart/ViewController.h
@@ -13,11 +13,12 @@
 #import "itemsViewController.h"
 #import "Product.h"
 
-@interface ViewController : UIViewController <FoodDelegate, DrinkDelegate, ClothDelegate, ItemDelegate>
+@interface ViewController : UIViewController <FoodDelegate, DrinkDelegate, ClothDelegate, ItemDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) NSMutableArray<Product*> *items;
 @property float sum;
 @property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UITableView *homeTableView;
 
 - (void) showSumPrice:(Product *)item;
 
